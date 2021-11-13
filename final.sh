08a1d3b6b2e207ed77f4267bdf5ef647cfc63dfa
Inok <inok.dr189@gmail.com>
2021-11-13 11:32:04 +0700

    Initial Commit

---
+++ final.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /tmp/rom
+
+. build/envsetup.sh
+lunch aosp_juice-userdebug
+curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G
+ccache -o compression=true
+ccache -z
+mka bacon -j8
