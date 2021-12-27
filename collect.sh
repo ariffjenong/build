e97f851c2c644acceba56867bc991e4fd817d92f
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-27 08:17:56 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -7,6 +7,7 @@ curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export ALLOW_MISSING_DEPENDENCIES=true
 ccache -M 20G
 ccache -o compression=true
 ccache -z
