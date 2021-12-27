a44ca1a06bb45afd2a9884f82560cbc40b4e063e
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-27 10:26:07 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -8,6 +8,7 @@ export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOW=true
 ccache -M 20G
 ccache -o compression=true
 ccache -z
