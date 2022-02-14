4131701ee3cac8ce369e79cf61a90bd83192b0ea
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-15 05:11:54 +0700

    BLISS ROM

--- final.sh
+++ final.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-cd /tmp/rom
+cd ~/rom
 
 . build/envsetup.sh
-lunch $LUNCH
+blissify -v $LUNCH
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-export CCACHE_DIR=/tmp/ccache
-export CCACHE_EXEC=$(which ccache)
-export USE_CCACHE=1
-ccache -M 20G
-ccache -o compression=true
-ccache -z
-$MAKE -j8 &
