9723c8bc1c7ede03d71ab3f24c16dc93f4da33f0
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-19 22:15:15 +0700

    Bliss-lfy

--- final.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ~/rom
-
-. build/envsetup.sh
-blissify -v $LUNCH
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
