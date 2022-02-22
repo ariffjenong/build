cb75d083db1b3a5799ad25b68e098fd1d30b6a2e
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-22 17:39:43 +0700

    Bliss-lfy

--- build_roms.sh
+++ build_roms.sh
@@ -11,7 +11,8 @@ env
  export USE_CCACHE=1 >> ~/.bashrc
  ccache -M 40G
  . build/envsetup.sh && export ALLOW_MISSING_DEPENDENCIES=true
+ lunch bliss_maple_dsds-userdebug
  curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
  export SELINUX_IGNORE_NEVERALLOWS=true
- blissify -v $LUNCH && export SELINUX_IGNORE_NEVERALLOWS=true && curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
+ blissify -v maple_dsds && export SELINUX_IGNORE_NEVERALLOWS=true && curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
  export SELINUX_IGNORE_NEVERALLOWS=true
