c94442f4185f526dfb9f946b48cc038374a9e0e0
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-25 12:33:22 +0700

    Nad-12

--- build_roms.sh
+++ build_roms.sh
@@ -11,11 +11,10 @@ env
  lunch nad_maple_dsds-userdebug
  export CCACHE_DIR=~/ccache
  export CCACHE_EXEC=$(which ccache)
- export USE_CCACHE=1
+ export USE_CCACHE=1 >> ~/ccache/.bashrc
  ccache -M 20G # It took only 6.4GB for mido
  ccache -o compression=true # Will save times and data to download and upload ccache, also negligible performance issue
  ccache -z # Clear old stats, so monitor script will provide real ccache statistics
  export ALLOW_MISSING_DEPENDENCIES=true
  export SELINUX_IGNORE_NEVERALLOWS=true
  curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
- make nad
--- ziping.sh
+++ ziping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/rom
+cd ~/ccache
 
 com ()
 {
