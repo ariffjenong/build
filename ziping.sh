70215d5b00d1d1a6275f276b62834d564b2b0b38
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-25 14:20:09 +0700

    Nad-12

--- ziping.sh
+++ ziping.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/znxt/ccache
+cd ~/znxt
 
 sleep 1m
 
@@ -10,3 +10,7 @@ com ()
 }
 
 time com ccache 1
+
+curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
+time rclone copy ccache.tar.gz znxt:ccache/maple_dsds -P
+curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache Success"
