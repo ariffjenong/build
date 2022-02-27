d68137e0e3f4329f985e2b8a133ff44b21948920
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-28 02:09:52 +0700

    nad-12: test build

--- build_zip.sh
+++ build_zip.sh
@@ -1,18 +1,18 @@
-#!/bin/bash
-#cd ~/znxt
+!/bin/bash
+cd ~/znxt
 
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 
-#time com ccache 1
+time com ccache 1
 
 
-#echo "$rclone_config" > ~/.config/rclone/rclone.conf
+echo "$rclone_config" > ~/.config/rclone/rclone.conf
 #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-#time rclone copy ccache.tar.gz znxt:ccache/nad
+time rclone copy ccache.tar.gz znxt:ccache/nad
 #cd ~/rom
 #time com out 6
-time rclone copy out.tar.gz znxt:test
-#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache Success"
+#time rclone copy out.tar.gz znxt:test
+curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache Success"
