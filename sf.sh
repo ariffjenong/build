d54fa866efc39d5242f9950ad5061dc0c8eebba8
ariffjenong <arifbuditantodablekk@gmail.com>
2022-02-28 15:19:05 +0700

    nad-12: test

--- sf.sh
+++ sf.sh
@@ -12,7 +12,7 @@
 	expect \"Password\"
 	send \"$SF_PASS\r\"
 	expect \"sftp> \"
-	send \"mkdir '- p /home/pfs/project/znxt-project/maple_dsds/nusantara-11'\r\"
+	send \"mkdir '-p /home/pfs/project/znxt-project/maple_dsds/nusantara-11'\r\"
 	set timeout -1
 	expect \"sftp>\"
 	send \"cd '/home/pfs/project/znxt-project/maple_dsds/nusantara-11'\r\"
@@ -21,17 +21,6 @@
 	expect \"Uploading\"
 	expect \"100%\"
 	expect \"sftp>\"
-	send \"bye\r\"
-	interact"
-
-	# Upload
-	expect -c "
-	spawn sftp $SF_USERNAME@frs.sourceforge.net
-	expect \"yes/no\"
-	send \"yes\r\"
-	expect \"Password\"
-	send \"$SF_PASS\r\"
-	expect \"sftp> \"
 	send \"mkdir '-p /home/pfs/project/znxt-project/maple_dsds/arrow-12.0'\r\"
 	set timeout -1
 	expect \"sftp>\"
