94fbc3cbe8e3983d85f4b1c9b7e0298c975943f0
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-03 11:38:18 +0700

    maple_dsds: test

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ make bootimage -j24
 make init -j24 &
 #make nad -j24 &  #dont remove that '&'
 #sleep 85m #first running
-sleep 102m #second running
-kill %1
+#sleep 102m #second running
+#kill %1
 
 #make nad -j30  \
 #      &&  curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build $(cd ~/rom/out/target/product/maple_dsds/ && ls *maple_dsds*UNOFFICIAL*.zip) Completed!"
