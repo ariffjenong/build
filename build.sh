625b6cf3ef0854d7cd677e482e12c659c678b9b5
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-03 13:02:57 +0700

    maple_dsds: test

--- build.sh
+++ build.sh
@@ -18,13 +18,13 @@ env
  export ALLOW_MISSING_DEPENDENCIES=true
  export TZ=Asia/Jakarta
  #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-make sepolicy -j24
-make bootimage -j24
-make init -j24 &
-#make nad -j24 &  #dont remove that '&'
+#make sepolicy -j24
+#make bootimage -j24
+#make init -j24 &
+make nad -j24 &  #dont remove that '&'
 #sleep 85m #first running
-#sleep 102m #second running
-#kill %1
+sleep 102m #second running
+kill %1
 
 #make nad -j30  \
 #      &&  curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build $(cd ~/rom/out/target/product/maple_dsds/ && ls *maple_dsds*UNOFFICIAL*.zip) Completed!"
