5dd91405d5157dafd8ece437d7c8cc28a841adaa
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-09 16:03:53 +0700

    test

--- build_zip.sh
+++ build_zip.sh
@@ -6,12 +6,12 @@ com ()
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 
-time com .git 7
+time com git 6
 
 
 #echo "$rclone_config" > ~/.config/rclone/rclone.conf
 #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-time rclone copy .git.tar.gz znxtproject:git -P
+time rclone copy git.tar.gz znxtproject:git -P
 #cd ~
 #time com rom 1
 ls -lh
