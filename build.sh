cc4c7ea17d33d5000ec3b37af740739ea5ebadc6
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-10 00:59:31 +0700

    cherish

--- build.sh
+++ build.sh
@@ -19,13 +19,13 @@ env
  export TZ=Asia/Jakarta
  #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 make sepolicy -j24
-#make bootimage -j24
-#make init -j24
-#make services
+make bootimage -j24
+make init -j24
+make services
 #make art
 brunch maple_dsds &  #dont remove that '&'
-#sleep 45m #first running
-sleep 90m #second running
+sleep 50m #first running
+#sleep 90m #second running
 #sleep 99m #third running
 kill %1
 
