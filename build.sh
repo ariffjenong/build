05f115d76d1a6259b2e531082130de4328d908be
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-09 20:09:01 +0700

    bliss

--- build.sh
+++ build.sh
@@ -15,22 +15,20 @@ env
  export USE_CCACHE=1
  ccache -M 8G
  ccache -z
- export ALLOW_MISSING_DEPENDENCIES=true
- PRODUCT_PACKAGES += \
-    vendor.lineage.touch*
+ export ALLOW_MISSING_DEPENDENCIES=true    
  export TZ=Asia/Jakarta
  #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-make sepolicy -j24
-make bootimage -j24
-make init -j24
-make services
-make art
+#make sepolicy -j24
+#make bootimage -j24
+#make init -j24
+#make services
+#make art
 blissify -v maple_dsds -j24 &  #dont remove that '&'
-sleep 45m #first running
-#sleep 100m #second running
+#sleep 45m #first running
+sleep 99m #second running
 kill %1
 
-#make nad -j30  \ #finall
+#blissify -v maple_dsds -j24 \ #finall
 #      &&  curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build $(cd ~/rom/out/target/product/maple_dsds/ && ls *maple_dsds*UNOFFICIAL*.zip) Completed!"
       
 ccache -s
