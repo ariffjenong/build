21a57755b18c027e7ce413d84cf7d254f59e4669
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-10 20:23:33 +0700

    Blissify

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ env
  export BUILD_HOSTNAME=ArifJeNong
  export BUILD_USERNAME=ArifJeNong
  export TZ=Asia/Jakarta
- #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
+#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 #make sepolicy -j24
 #make bootimage -j24
 #make init -j24
