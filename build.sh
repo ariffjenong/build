9d2727d23887df2730bead06b98d61f06bee2ace
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-14 08:09:09 +0700

    test

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ env
 
  cd ~/rom
  . build/envsetup.sh
- lunch cherish_maple-userdebug
- #export SELINUX_IGNORE_NEVERALLOWS=true
+ lunch lineage_maple_dsds-userdebug
+ export SELINUX_IGNORE_NEVERALLOWS=true
  export CCACHE_DIR=~/znxt/ccache
  export CCACHE_EXEC=$(which ccache)
  export USE_CCACHE=1
@@ -21,18 +21,19 @@ env
  export BUILD_USERNAME=ArifJeNong
  export TZ=Asia/Jakarta
  #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-#make sepolicy -j24
-#make bootimage -j24
-#make init -j24
-#make services
-#make art
-#brunch maple &  #dont remove that '&'
+make sepolicy -j24
+make bootimage -j24
+make init -j24
+make services
+make art
+make frameworks
+make bacon -j12 &  #dont remove that '&'
 #sleep 50m #first running
 #sleep 99m #second running
 #sleep 102m #third running
 #kill %1
 
-brunch maple
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build $(cd ~/rom/out/target/product/maple/ && ls *maple*UNOFFICIAL*.zip) Completed!"
+#make bacon -j12
+#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Build $(cd ~/rom/out/target/product/maple_dsds/ && ls *maple*UNOFFICIAL*.zip) Completed!"
       
 ccache -s
