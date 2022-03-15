09a83f36e855536e7137c31f584d3faa702fdb6f
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-15 11:08:07 +0700

    19.1

--- ziper.sh
+++ ziper.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-cd ~/$ROM_PROJECT
+cd ~
 
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 
-rm -rf ~/$ROM_PROJECT/out/target/product/maple_dsds
-
-time com out 1
+time com $ROM_PROJECT 9
 
 
 #echo "$rclone_config" > ~/.config/rclone/rclone.conf
 #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-time rclone copy out.tar.gz znxtproject:ccache/$ROM_PROJECT -P
+time rclone copy $ROM_PROJECT.tar.gz znxtproject:ccache/$ROM_PROJECT -P
 #cd ~
 #time com rom 1
 ls -lh
