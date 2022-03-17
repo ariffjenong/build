6145a5de1fea50f4d2d8c4e2eb4e4cad6dfcc0a8
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-18 00:51:46 +0700

    test build one

--- repo.sh
+++ repo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd ~/$ROM_PROJECT
-rm -fv .repo/local_manifests/*
-wget https://raw.githubusercontent.com/ariffjenong/local_manifest/LOS19/roomservices.xml -O .repo/local_manifests/roomservices.xml
+rm -rf .repo/local_manifests
 repo init --depth=1 --no-repo-verify --partial-clone --clone-filter=blob:limit=10M -u https://github.com/ariffjenong/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b LOS19 .repo/local_manifests
 repo forall -j"$(nproc)" -c "git reset --hard m/pie && git clean -fdx"
+repo sync frameworks/base -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
