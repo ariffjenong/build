68bce5875780d6e660c6bb11c18ce6fd45c7f77c
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-17 23:17:02 +0700

    test build

--- repo.sh
+++ repo.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 cd ~/$ROM_PROJECT
-repo init --depth=1 --no-repo-verify -u https://github.com/ariffjenong/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b LOS19 .repo/local_manifests
-cd .repo/manifests
-sed -i '/darwin/d' default.xml
-( find . -type f -name '*.xml' | xargs sed -i '/darwin/d' ) || true
-git commit -a -m "Bump" || true
-cd ../
-sed -i '/darwin/d' manifest.xml
-cd ../
+rm -fv .repo/local_manifests/*
+repo init --depth=1 --no-repo-verify --partial-clone --clone-filter=blob:limit=10M -u https://github.com/ariffjenong/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+wget https://raw.githubusercontent.com/ariffjenong/local_manifest/LOS19/roomservices.xml -O .repo/local_manifests/roomservices.xml
+repo forall -j"$(nproc)" -c "git reset --hard m/pie && git clean -fdx"
+repo sync frameworks/base
