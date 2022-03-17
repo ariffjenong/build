e8a2be04ffa3a515eec902698146c2b2799b5f8f
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-18 01:43:04 +0700

    test build one

--- repo.sh
+++ repo.sh
@@ -2,6 +2,6 @@
 
 cd ~/$ROM_PROJECT
 rm -rf .repo/local_manifests
-repo init --depth=1 --no-repo-verify --partial-clone --clone-filter=blob:limit=10M -u https://github.com/ariffjenong/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/ariffjenong/android.git -b lineage-19.1 -g default,-mips,-darwin,-notdefault
 git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b LOS19 .repo/local_manifests
 repo forall -j"$(nproc)" -c "git reset --hard m/pie && git clean -fdx"
