bd417e644bac7b52e8f04e7425743e40b35f7237
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-18 12:57:05 +0700

    fix sync error

--- ziper.sh
+++ ziper.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 cd ~/$ROM_PROJECT/out/target/product/maple_dsds
 
+rm *txt *json *mk
+time rm -rf system kernel root recovery ramdisk cache debug_ramdisk
+
+cd ~/$ROM_PROJECT/out/target/product
+
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 
-time com obj 5
+time com maple_dsds 5
 
 
-#echo "$rclone_config" > ~/.config/rclone/rclone.conf
-#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-time rclone copy obj.tar.gz znxtproject:ccache/$ROM_PROJECT -P
-#cd ~
-#time com rom 1
+time rclone copy maple_dsds.tar.gz znxtproject:ccache/$ROM_PROJECT -P
 ls -lh
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading Success"
