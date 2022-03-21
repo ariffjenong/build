3919aa163f2b392f51ded0ba3bc75f80673761db
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-21 13:10:33 +0700

    los19

--- retryCc.sh
+++ retryCc.sh
@@ -12,7 +12,7 @@ retry_ccache () {
 	export CCACHE_EXEC=$(which ccache)
 	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
 	if [ $hit_rate -lt 99 ]; then
-		git clone --depth=1 ${TOKEN}/ariffjenong/build.git -b Builder cirrus && cd $_
+		git clone ${TOKEN}/ariffjenong/build -b cherish-12.1_new cirrus && cd $_
 		git commit --allow-empty -m "Retry: Ccache loop $(date -u +"%D %T%p %Z")"
 		git push -q
 	else
@@ -29,6 +29,6 @@ retry_event() {
 	fi
 }
 
-cd /tmp/rom && sleep 113m
+cd /tmp/rom && sleep 119m
 compiled_zip
 retry_event
