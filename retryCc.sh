b39b912e88a9aecf49acbb93d37c1360e27bbf73
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-22 13:22:50 +0700

    revert

--- retryCc.sh
+++ retryCc.sh
@@ -13,7 +13,7 @@ retry_ccache () {
 	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
 	if [ $hit_rate -lt 99 ]; then
 		git clone ${TOKEN}/ariffjenong/build -b cherish-12.1_new cirrus && cd $_
-		folder=$(pwd)
+		folder=/tmp/rom/cirrus
 		rm retryCc.sh download.sh
 		time rclone copy znxtproject:github/retryCc.sh $folder -P
 		time rclone copy znxtproject:github/download.sh $folder -P
