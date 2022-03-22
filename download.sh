584d57bfaaf9de99fa5828ab7c5817c8d265d3ed
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-22 10:50:44 +0700

    change Ccache compression to .gz

--- download.sh
+++ download.sh
@@ -3,8 +3,8 @@
 ccache_download () {
 	mkdir -p ~/.config/rclone
 	echo "$rclone_config" > ~/.config/rclone/rclone.conf
-	rclone copy znxtproject:ccache/$NAME/ccache.tar.zst /tmp -P
-	tar -xaf ccache.tar.zst
+	time rclone copy znxtproject:ccache/$NAME/ccache.tar.zst /tmp -P
+	time tar -xaf ccache.tar.zst
 	rm ccache.tar.zst
 }
 
