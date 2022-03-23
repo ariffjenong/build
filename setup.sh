570e5d076138c62ce54623947f064b97c4b434bc
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-24 03:15:45 +0700

    cherish

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 cd /cirrus
 ls -lh
 time tar xf ccache.tar.gz
-rm ccache.tar.gz brotli kati make ninja nsjail rclone-v1.58.0-linux-amd64 script zstd-1.5.2
+rm ccache.tar.gz
+rm -rf brotli kati make ninja nsjail rclone-v1.58.0-linux-amd64 script zstd-1.5.2
