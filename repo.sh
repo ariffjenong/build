46f55489bad3361ed04136130a31a1ee6777fa1b
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-24 01:50:32 +0700

    cherish

--- repo.sh
+++ repo.sh
@@ -2,7 +2,4 @@
 cd /cirrus/rom
 
 ls -lh .repo/local_manifests
-rm -rf .repo/local_manifests
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
