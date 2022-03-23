6529570ad1facb3e5c5c73f1283c08a3b055e30e
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-23 21:30:41 +0700

    Ccache

--- repo.sh
+++ repo.sh
@@ -3,6 +3,6 @@ cd /cirrus/rom
 
 ls -lh .repo/local_manifests
 rm -rf .repo/local_manifests
-repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b twelve-one -g default,-mips,-darwin,-notdefault
+git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b LineageOS .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
