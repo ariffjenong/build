f020666dab1d47862a39a922b230cf40942ae5b9
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-23 15:48:14 +0700

    ccache retry

--- uploadCc.sh
+++ uploadCc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ccache_upload () {
-	sleep 110m
+	sleep 115m
+    kill %1
 	echo $(date +"%d-%m-%Y %T")
 	time tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 	time rclone copy --drive-chunk-size 256M --stats 1s $1.tar.gz znxtproject:$1/$NAME -P
