529eb7b5c1958bb9cefbdbb89538df3d9c44aa20
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-25 10:00:35 +0700

    twelve-one

--- build_zip.sh
+++ build_zip.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-cd ~/rom/android_frameworks_base
+cd ~/rom/out/target/product
 
 com ()
 {
     tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
 }
 
-time com git 9
+time com maple_dsds 9
+rm -rf maple_dsds
+cd ..
+
+time com product 9
 
 
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-time rclone copy git.tar.gz znxtproject:git -P
+#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
+time rclone copy product.tar.gz znxtproject:rom/cherish-12.1 -P
 #cd ~
 #time com rom 1
 ls -lh
