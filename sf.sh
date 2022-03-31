d1fb866d01eb85620ed1fbfbeeddc7fcb7309996
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-31 09:47:56 +0700

    retry

--- sf.sh
+++ sf.sh
@@ -3,6 +3,36 @@
 
 sudo apt-get update -y && sudo apt-get install expect -y
 
+cd ~/rom/out/target/product/maple
+
+productGAPPS=$(ls *GApps*.zip)
+#productmaple=$(ls *Vanilla*.zip)
+projectmaple=xperia-xz-premium/maple
+
+# Upload
+expect -c "
+spawn sftp $SF_USERNAME@frs.sourceforge.net:/home/pfs/project/$projectmaple
+expect \"yes/no\"
+send \"yes\r\"
+expect \"sftp> \"
+send \"cd $ROM_PROJECT\r\"
+set timeout -1
+send \"put $productGAPPS\r\"
+expect \"Uploading\"
+expect \"100%\"
+expect \"sftp>\"
+send \"bye\r\"
+interact"
+
+# Post
+curl -s -v -F "chat_id=$TG_CHAT_ID" -F "parse_mode=html" -F text="Build completed successfully!!!
+Link: https://sourceforge.net/projects/xperia-xz-premium/files/maple/$ROM_PROJECT/$productmaple
+Dev : ""Arif JeNong""
+Product : ""$productmaple"" ""$productGAPPS""
+Device : ""maple""
+Server Host : cirrus-ci
+Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/$TG_TOKEN/sendMessage
+
 cd ~/rom/out/target/product/maple_dsds
 
 GAPPS=$(ls *GApps*.zip)
