1f30f2f5123c00004417a31cd77735510bb35c67
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-01 00:22:43 +0700

    use zstd [skip ci]

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /cirrus
 ls -lh
-time tar xf ccache.tar.gz
-time rm ccache.tar.gz
-#time tar -xaf ccache.tar.zst
-#time rm -rf ccache.tar.zst
+#time tar xf ccache.tar.gz
+#time rm ccache.tar.gz
+time tar -xaf ccache.tar.zst
+time rm -rf ccache.tar.zst
