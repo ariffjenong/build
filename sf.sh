e3cf268240434c6250c42426a815af9a1bf4233a
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-31 09:38:12 +0700

    retry

--- sf.sh
+++ sf.sh
@@ -16,7 +16,6 @@ expect \"yes/no\"
 send \"yes\r\"
 expect \"Password\"
 send \"${SF_PASS}\r\"
-set timeout -1
 expect \"sftp> \"
 send \"cd $ROM_PROJECT\r\"
 set timeout -1
@@ -37,35 +36,5 @@ Link: https://sourceforge.net/projects/xperia-xz-premium/files/maple_dsds/$ROM_P
 Dev : ""Arif JeNong""
 Product : ""$product"" ""$GAPPS""
 Device : ""maple_dsds""
-Server Host : cirrus-ci
-Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/$TG_TOKEN/sendMessage
-
-cd ~/rom/out/target/product/maple
-
-productGAPPS=$(ls *GApps*.zip)
-#productmaple=$(ls *Vanilla*.zip)
-projectmaple=xperia-xz-premium/maple
-
-# Upload
-expect -c "
-spawn sftp $SF_USERNAME@frs.sourceforge.net:/home/pfs/project/$projectmaple
-expect \"yes/no\"
-send \"yes\r\"
-expect \"sftp> \"
-send \"cd $ROM_PROJECT\r\"
-set timeout -1
-send \"put $productGAPPS\r\"
-expect \"Uploading\"
-expect \"100%\"
-expect \"sftp>\"
-send \"bye\r\"
-interact"
-
-# Post
-curl -s -v -F "chat_id=$TG_CHAT_ID" -F "parse_mode=html" -F text="Build completed successfully!!!
-Link: https://sourceforge.net/projects/xperia-xz-premium/files/maple/$ROM_PROJECT/$productmaple
-Dev : ""Arif JeNong""
-Product : ""$productmaple"" ""$productGAPPS""
-Device : ""maple""
 Server Host : cirrus-ci
 Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/$TG_TOKEN/sendMessage
