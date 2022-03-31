e808551e8112ab3d8f960c3a6daab5b20620d9e6
ariffjenong <arifbuditantodablekk@gmail.com>
2022-03-31 08:56:03 +0700

    uploading

--- sf.sh
+++ sf.sh
@@ -5,8 +5,8 @@ cd ~/rom/out/target/product/maple_dsds
 
 sudo apt-get update -y && sudo apt-get install expect -y
 
-#product=$(ls *maple_dsds*Gapps*.zip)
-product=$(ls *UNOFFICIAL*.zip)
+GAPPS=$(ls *GApps*.zip)
+product=$(ls *VANILLA*.zip)
 project=xperia-xz-premium/maple_dsds
 
 # Upload
@@ -17,12 +17,13 @@ send \"yes\r\"
 expect \"Password\"
 send \"$SF_PASS\r\"
 expect \"sftp> \"
-send \"mkdir $ROM_PROJECT\r\"
-set timeout -1
-expect \"sftp> \"
 send \"cd $ROM_PROJECT\r\"
 set timeout -1
-send \"put $product\r\"
+send \"put $VANILLA\r\"
+expect \"Uploading\"
+expect \"100%\"
+expect \"sftp>\"
+send \"put $GAPPS\r\"
 expect \"Uploading\"
 expect \"100%\"
 expect \"sftp>\"
@@ -33,7 +34,37 @@ interact"
 curl -s -v -F "chat_id=$TG_CHAT_ID" -F "parse_mode=html" -F text="Build completed successfully!!!
 Link: https://sourceforge.net/projects/xperia-xz-premium/files/maple_dsds/$ROM_PROJECT/$product
 Dev : ""Arif JeNong""
-Product : ""$product""
+Product : ""$product"" ""$GAPPS""
 Device : ""maple_dsds""
+Server Host : cirrus-ci
+Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/$TG_TOKEN/sendMessage
+
+cd ~/rom/out/target/product/maple
+
+productGAPPS=$(ls *GApps*.zip)
+#productmaple=$(ls *VANILLA*.zip)
+projectmaple=xperia-xz-premium/maple
+
+# Upload
+expect -c "
+spawn sftp $SF_USERNAME@frs.sourceforge.net:/home/pfs/project/$projectmaple
+expect \"yes/no\"
+send \"yes\r\"
+expect \"sftp> \"
+send \"cd $ROM_PROJECT\r\"
+set timeout -1
+send \"put $productGAPPS\r\"
+expect \"Uploading\"
+expect \"100%\"
+expect \"sftp>\"
+send \"bye\r\"
+interact"
+
+# Post
+curl -s -v -F "chat_id=$TG_CHAT_ID" -F "parse_mode=html" -F text="Build completed successfully!!!
+Link: https://sourceforge.net/projects/xperia-xz-premium/files/maple/$ROM_PROJECT/$productmaple
+Dev : ""Arif JeNong""
+Product : ""$productmaple"" ""$productGAPPS""
+Device : ""maple""
 Server Host : cirrus-ci
 Date : ""$(env TZ=Asia/Jakarta date)""" https://api.telegram.org/$TG_TOKEN/sendMessage
