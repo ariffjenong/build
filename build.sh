2eeb524d59ec6a36385fa86ee0eb8fbacfd36a42
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-01 11:59:31 +0700

    Initial commit [skip ci]

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Lets see machine specifications and environments
+df -h
+free -h
+nproc
+cat /etc/os*
+env
+
+ cd /cirrus/rom
+ . build/envsetup.sh
+ lunch dot_maple_dsds-userdebug
+ export CCACHE_DIR=/cirrus/ccache
+ export CCACHE_EXEC=$(which ccache)
+ export USE_CCACHE=1
+ ccache -M 50G
+ ccache -z
+ #export ALLOW_MISSING_DEPENDENCIES=true
+ #export SELINUX_IGNORE_NEVERALLOWS=true
+ export BUILD_HOSTNAME=ArifJeNong
+ export BUILD_USERNAME=ArifJeNong
+ export WITH_GAPPS=true
+ export TZ=Asia/Jakarta
+ #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
+#make sepolicy -j24
+#make bootimage -j24
+#make init -j24
+#make services
+#make systemimage &
+make bacon -j24 & #dont remove that '&'
+#sleep 50m #first running
+sleep 104m #second running
+#sleep 106m #third running
+kill %1
+ 
+#make bacon -j24
+ccache -s
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd /cirrus
+ls -lh
+#time tar xf ccache.tar.gz
+#time rm ccache.tar.gz
+time tar -xaf ccache.tar.zst
+time rm -rf ccache.tar.zst
