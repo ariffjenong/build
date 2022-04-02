aa9c7b2fd69b18ad3157d96f9945c8865606c8c6
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 03:30:18 +0700

    edit

--- build_zip.sh
+++ build_zip.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-com ()
-{
+com () {
     #tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
     tar "-I zstd -1 -T2" -cf $1.tar.zst $1
 }
@@ -9,17 +8,9 @@ com ()
 get_ccache () {
 cd /cirrus
 time com ccache 1
-
-
-
-#echo "$rclone_config" > ~/.config/rclone/rclone.conf
-#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
 time rclone copy ccache.tar.* znxtproject:ccache/$ROM_PROJECT -P
-#cd ~
-#time com rom 1
 time rm ccache.tar.*
 ls -lh
-#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading Success"
 }
 
 system () {
