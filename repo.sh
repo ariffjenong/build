e7ec5a4446c83e02e6bbb044beef7709cf3d597b
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 22:01:16 +0700

    apply patch for maple_dsds [skip ci]

--- repo.sh
+++ repo.sh
@@ -10,15 +10,22 @@ sync () {
 
 compile () {
     sync
-    echo "done"
+    echo "done."
+}
+
+patch () {
+  cd device/sony/maple_dsds
+  wget https://raw.githubusercontent.com/Flamefire/android_device_sony_lilac/lineage-19.0/patches/applyPatches.sh
+  wget https://raw.githubusercontent.com/Flamefire/android_device_sony_lilac/lineage-19.0/patches/workaround_egl_lib_symbols.patch
+  git add . && git commit -m 'apply path'
 }
 
 ls -lh
 compile
+patch
 
 # Lets see machine specifications and environments
 df -h
 free -h
 nproc
 cat /etc/os*
-env
