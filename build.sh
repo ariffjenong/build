f06fb43237903c2189fd4f3fb022f5649b8e865b
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-05 17:15:07 +0700

    make sytem [skip ci]

--- build.sh
+++ build.sh
@@ -8,18 +8,14 @@
  export USE_CCACHE=1
  ccache -M 50G
  ccache -z
- #export ALLOW_MISSING_DEPENDENCIES=true
  export USE_GAPPS=true
  export TZ=Asia/Jakarta
- #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
-#make sepolicy -j24
-#make bootimage -j24
-#make init -j24
-#make services
-make nad &  #dont remove that '&'
-#sleep 83m #first running
-#sleep 100m #second running
-sleep 106m #third running
+make systemimage &
+#make nad &  #dont remove that '&'
+#sleep 107m #first running
+#sleep 106m #second running
+sleep 105m #third running
+#sleep 104m #fourth running
 kill %1
 
 #make nad -j30 #finall
