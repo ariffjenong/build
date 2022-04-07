891766c74454de1964606bf1fdc5d9da20b9fa8b
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-07 16:05:09 +0700

    Blissify 104m [skip ci]

--- build.sh
+++ build.sh
@@ -8,14 +8,16 @@
  export USE_CCACHE=1
  ccache -M 50G
  ccache -z
+ export BUILD_HOSTNAME=znxt
+ export BUILD_USERNAME=znxt
  #export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
  export TZ=Asia/Jakarta
 blissify -g maple_dsds &
 #make nad &  #dont remove that '&'
 #sleep 108m #first running
-sleep 106m #second running
+#sleep 106m #second running
 #sleep 105m #third running
-#sleep 104m #fourth running
+sleep 104m #fourth running
 kill %1
 
 #blissify -g maple_dsds #finall
