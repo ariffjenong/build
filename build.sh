aae4d51179747d4054fc4b379908ddcb2cdf206d
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-09 20:40:08 +0700

    get out

--- build.sh
+++ build.sh
@@ -19,11 +19,11 @@ env
  export WITH_GAPPS=true
  export TZ=Asia/Jakarta
 #make systemimage &
-#make installclean
+make installclean
 make bacon -j24 & #dont remove that '&'
 #sleep 50m #first running
-#sleep 90m #second running
-sleep 104m #third running
+sleep 90m #second running
+#sleep 104m #third running
 kill %1
  
 #make bacon -j24
--- build_zip.sh
+++ build_zip.sh
@@ -7,15 +7,15 @@ com ()
     tar "-I zstd -1 -T2" -cf $1.tar.zst $1
 }
 
-time com ccache 1
+#time com ccache 1
 
 
 #echo "$rclone_config" > ~/.config/rclone/rclone.conf
 #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-time rclone copy ccache.tar.* znxtproject:ccache/$ROM_PROJECT -P
+#time rclone copy ccache.tar.* znxtproject:ccache/$ROM_PROJECT -P
 #cd ~
 #time com rom 1
-time rm ccache.tar.*
+#time rm ccache.tar.*
 ls -lh
 
 get_out () {
@@ -26,6 +26,6 @@ get_out () {
   ls -lh
 }
 
-#get_out
+get_out
 
 #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading Success"
